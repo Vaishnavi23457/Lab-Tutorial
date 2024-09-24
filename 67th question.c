c2c644c4ff54a0486b80ae82166b72c0d1735920
Vaishnavi23457 <kachigallavaishnavi@gmail.com>
2024-09-24 19:00:50 +0530

    Add files via upload

---
+++ 67th question.c
@@ -0,0 +1,51 @@
+
+int main()
+{
+	int a[3][3],b[3][3],c[3][3],i,j;
+	printf("Enter the elements of the matrix:");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+		scanf("%d",&a[i][j]);
+		}
+	}
+	printf("value of the matrix\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+		printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\nEnter the value of matrix:");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+		scanf("%d",&b[i][j]);
+		}
+	}
+	printf("value of the matrix\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+		printf("%d\t",b[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n Substraction of matrix b from a:");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+		c[i][j]=a[i][j]-b[i][j];
+		printf("%d\t",c[i][j]);
+		}
+		printf("\n");
+	}
+			
+}
+
