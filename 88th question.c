12fc22afd48a4863a89879f73825c04ec4143968
Vaishnavi23457 <kachigallavaishnavi@gmail.com>
2024-11-20 09:11:35 +0530

    Add files via upload

---
+++ 88th question.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void copyArray(int *source, int *dest, int size) {
+    for (int i = 0; i < size; i++) {
+        *(dest + i) = *(source + i);
+    }
+}
+
+int main() {
+    int source[] = {1, 2, 3, 4, 5};
+    int size = sizeof(source) / sizeof(source[0]);
+    int dest[size];
+
+    copyArray(source, dest, size);
+
+    printf("Copied Array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", dest[i]);
+    }
+
+    return 0;
+}
